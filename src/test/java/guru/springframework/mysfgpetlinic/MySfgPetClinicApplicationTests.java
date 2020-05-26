c0d436a2f0f66474b29b34e0ed3892e4e300bd1f
rveigarcia <rveigarcia@gmail.com>
2020-05-26 19:18:48 +0200

    Añadiendo estructura de proyecto spring al repositorio

---
+++ src/test/java/guru/springframework/mysfgpetlinic/MySfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.mysfgpetlinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
